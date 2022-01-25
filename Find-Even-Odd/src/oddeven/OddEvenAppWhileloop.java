01afba1e22e2ab116a807fdda05e2a5e14fe96ba
Yota2 <yoni.jmr@gmail.com>
2022-01-25 14:49:56 +0100

    OddEvenwhile

---
+++ Find-Even-Odd/src/oddeven/OddEvenAppWhileloop.java
@@ -0,0 +1,24 @@
+package oddeven;
+import java.util.*;
+public class OddEvenAppWhileloop {
+    public static void main(String[] args) {
+        System.out.println("--Get even and odd under 100");
+
+            int i = 1;
+            while (i < 100) {
+                if(i % 2 == 0)
+                    System.out.println(i + " is even");
+                else
+                    System.out.println(i + " is odd");
+                i++;
+            }
+            };
+
+    }
+
+
+
+
+
+
+
