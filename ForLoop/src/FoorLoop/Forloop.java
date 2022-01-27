899136caa6edc7e698dad20d4b512d45dab28ba8
Yota2 <yoni.jmr@gmail.com>
2022-01-27 10:20:38 +0100

    Foorloops

---
+++ ForLoop/src/FoorLoop/Forloop.java
@@ -0,0 +1,12 @@
+package FoorLoop;
+
+public class Forloop {
+    public static void main(String args[])
+    {
+        // Writing a for loop
+
+        for (int j = -1; j <= 29 ; j += 3) {
+            System.out.println(j);
+        }
+    }
+}
