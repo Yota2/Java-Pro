4a8313cbeda4957c4d8442d1e463f581dcbfd183
Yota2 <yoni.jmr@gmail.com>
2022-02-01 21:45:43 +0100

    SaturdayAndSunday-Salary

--- Tijdmeting-App/src/TijdmetingApp.java
+++ Tijdmeting-App/src/TijdmetingApp.java
@@ -5,11 +5,13 @@ import java.text.*;
 public class TijdmetingApp {
     public static void main(String[] args) {
         // getting input from the user
-        double salary;
         double startHour;
-        //int totalminutes;
-
-        double salaryPerMinutes = (11.5*100)/60;
+        double stophours;
+        double totaltimeWork=0;
+        double worktotalhour ,totalminutes;
+        double salaryPerMinutes = 11.5/60;
+        double salaryPerMinuteSaturday = (11.5*1.25)/60;
+        double salaryPerMinuteSunday = (11.5*1.50)/60;
         Scanner userinp = new Scanner(System.in);
         do {
             System.out.println("Fill the hour you started working  from [0-23]");
@@ -21,125 +23,85 @@ public class TijdmetingApp {
             System.out.println("Fill the minutes you started working  from [0-59]");
             startminutes = userinp.nextInt();
         } while (startminutes < 0 || startminutes > 59);
-
-        double stophours;
         do {
             System.out.println("Fill the hours you stopped working  from [0-23]");
             stophours = userinp.nextInt();
         } while (stophours < 0 || stophours > 23);
-
         int stopminute;
         do {
             System.out.println("Fill the minute you stopped working  from [0-59]");
             stopminute = userinp.nextInt();
         } while (stopminute < 0 || stopminute > 59);
-
-        double worktotalhour ,totalminutes, dayhour, nighthour, nightminutes,midinighthour, midinightminutes ,saturdaySalary,
-                sundaySalary , fridaysalary ,bonsFriday,friDayhour;
-        /*
-        double totalminutes;
-        double dayhour;
-        double nighthour;
-        double nightminutes;
-        double midinighthour;
-        double midinightminutes;
-        */
-        // calc night-hour
-
-       /* if (startHour >= 18 ) {
-            nighthour = 24 - startHour;
-            worktotalhour = nighthour + stophours;
-       } else if (startHour >= 7 ) {
+        if (startHour > stophours) {
+            worktotalhour =  stophours - startHour + 24;
+        } else if (startHour < stophours) {
             worktotalhour = (stophours - startHour);
         } else {
             worktotalhour = 0;
         }
-
-        */
-
-            if (startHour > stophours) {
-                worktotalhour =  stophours - startHour + 24;
-            } else if (startHour < stophours) {
-                worktotalhour = (stophours - startHour);
-            } else {
-                worktotalhour = 0;
+        if (startminutes > stopminute) {
+            totalminutes = stopminute - startminutes + 60;
+            worktotalhour--;
+        } else if (startminutes < stopminute) {
+            totalminutes = (stopminute - startminutes );
+        } else {
+            totalminutes = 0;
+        }
+        System.out.println("Please Fill in the number of the dy you started working");
+        int day;
+        System.out.println("Monday [1]");
+        System.out.println("Tuesday [2]");
+        System.out.println("Wednesday [3]");
+        System.out.println("Thursday [4]");
+        System.out.println("Friday [5]");
+        System.out.println("Saturday [6]");
+        System.out.println("Sunday [7]");
+        // cal the money per day
+        day = userinp.nextInt();
+        // calculations
+        double totaltimeWorkBasic, salaryBasic, totalTimeOvertime, salaryOvertime, totalSalary = 0;
+        if(day >= 1 && day <=4 ) {
+            totaltimeWork = worktotalhour * 60 + totalminutes;
+            totalSalary = (double) (salaryPerMinutes * totaltimeWork);
+        } else if(day == 5 ) {
+            if(stophours < startHour ) {
+                totaltimeWorkBasic = (24-startHour)*60 - startminutes;
+                salaryBasic = (double) (salaryPerMinutes * totaltimeWorkBasic);
+                totalTimeOvertime = stophours * 60 + stopminute;
+                salaryOvertime = (double) ((salaryPerMinuteSaturday) * totalTimeOvertime);
+                totalSalary = salaryBasic+salaryOvertime;
+            }else {
+                totaltimeWork = worktotalhour * 60 + totalminutes;
+                totalSalary = (double) (salaryPerMinutes * totaltimeWork);
+                System.out.println("worktime : " +totaltimeWork);
+                System.out.println("paid friday : " + salaryPerMinutes);
             }
-            if (startminutes > stopminute) {
-                totalminutes = stopminute - startminutes + 60;
-                worktotalhour--;
-            } else if (startminutes < stopminute) {
-                totalminutes = (stopminute - startminutes );
-            } else {
-                totalminutes = 0;
+        } else if(day == 6 ){
+            if(stophours < startHour ) {
+                totaltimeWorkBasic = (24-startHour)*60 - startminutes;
+                salaryBasic = (double) ((salaryPerMinuteSaturday) * totaltimeWorkBasic);
+                totalTimeOvertime = stophours * 60 + stopminute;
+                salaryOvertime = (double) ((salaryPerMinuteSunday) * totalTimeOvertime);
+                totalSalary = salaryBasic+salaryOvertime;
+            }else {
+                totaltimeWork = worktotalhour * 60 + totalminutes;
+                totalSalary = (double) ((salaryPerMinuteSaturday) * totaltimeWork);
             }
-
-
-            double totaltimeWork = worktotalhour * 60 + totalminutes;
-
-            System.out.println("Please Fill in the number of the dy you started working");
-
-            int day;
-            System.out.println("Monday [1]");
-            System.out.println("Tuesday [2]");
-            System.out.println("Wednesday [3]");
-            System.out.println("Thursday [4]");
-            System.out.println("Friday [5]");
-            System.out.println("Saturday [6]");
-            System.out.println("Sunday [7]");
-
-            // cal the money per day
-            day = userinp.nextInt();
-            switch (day) {
-                case 1:
-                    salary = (double) (salaryPerMinutes * totaltimeWork);
-                    System.out.println("Start uur " + "     \t     " + "Stop uur " + "     \t     " + "Startday " + "     \t     " + "Enday" + "     \t          " + "uur gewerkt" + "     \t      " + "Loon");
-                    System.out.print(startHour + "u" + startminutes + "m"+"     \t           " + stophours + "u" + startminutes + "m"+ "     \t       " + "Monday" + "      \t     " + "Monday" + "       \t        " + worktotalhour + "u"+ totalminutes + "m"+  "      \t	      " + salary/100 + "€");
-                    break;
-                case 2:
-                    salary = (double) (salaryPerMinutes * totaltimeWork);
-                    System.out.println("Start uur " + "     \t     " + "Stop uur " + "     \t     " + "Startday " + "     \t     " + "Enday" + "     \t          " + "uur gewerkt" + "     \t      " + "Loon");
-                    System.out.print(startHour + "u" + startminutes + "m"+ "     \t           " + stophours + "u" + startminutes + "m"+ "     \t       " + "Tuesday" + "      \t     " + "Tuesday" + "       \t        " + worktotalhour + "u"+ totalminutes + "m"+  "      \t	      " + salary/100 + "€");
-                    break;
-                case 3:
-                    salary = (double) (salaryPerMinutes * totaltimeWork);
-                    System.out.println("Start uur " + "     \t     " + "Stop uur " + "     \t     " + "Startday " + "     \t     " + "Enday" + "     \t          " + "uur gewerkt" + "     \t      " + "Loon");
-                    System.out.print(startHour + "u" + startminutes + "m"+ "     \t           " + stophours + "u" + startminutes + "m"+ "     \t       " + "Wednesday" + "      \t     " + "Wednesday" + "       \t        " + worktotalhour + "u"+ totalminutes + "m"+ "     \t	      " + salary/100 + "€");
-                    break;
-                case 4:
-                    salary = (double) (salaryPerMinutes * totaltimeWork);
-                    System.out.println("Start uur " + "     \t     " + "Stop uur " + "     \t     " + "Startday " + "     \t     " + "Enday" + "     \t          " + "uur gewerkt" + "     \t      " + "Loon");
-                    System.out.print(startHour + "u" + startminutes + "m"+ "     \t           " + stophours + "u" + startminutes + "m"+ "     \t       " + "Thursday" + "      \t     " + "Thursday" + "       \t        " + worktotalhour + "u"+ totalminutes + "m"+ "     \t	      " + salary/100 + "€");
-                    break;
-                case 5:
-
-                    if(stophours < 7 && stophours > 0.01 ){
-                        bonsFriday = (stophours*11.25)*1.25;
-                       // System.out.println(bonsFriday);
-                        friDayhour =( 24.0 - startHour)*11.25;
-                        totaltimeWork = friDayhour + bonsFriday;
-                        fridaysalary =  friDayhour +  bonsFriday;
-                       // System.out.println(fridaysalary);
-                        System.out.println("Start uur " + "     \t     " + "Stop uur " + "     \t     " + "Startday " + "     \t     " + "Enday" + "     \t          " + "uur gewerkt" + "     \t      " + "Loon");
-                        System.out.print(startHour + "u" + startminutes + "m" + "     \t           " + stophours + "u" + startminutes + "m" + "     \t       " + "Friday" + "      \t     " + "Friday" + "       \t        " + worktotalhour + "u" + totalminutes + "m" + "     \t	      " + fridaysalary  + "€");
-
-                        } else {
-                        fridaysalary = (double) (salaryPerMinutes * totaltimeWork);
-                        System.out.println("Start uur " + "     \t     " + "Stop uur " + "     \t     " + "Startday " + "     \t     " + "Enday" + "     \t          " + "uur gewerkt" + "     \t      " + "Loon");
-                        System.out.print(startHour + "u" + startminutes + "m" + "     \t           " + stophours + "u" + startminutes + "m" + "     \t       " + "Friday" + "      \t     " + "Friday" + "       \t        " + worktotalhour + "u" + totalminutes + "m" + "     \t	      " + fridaysalary /100 + "€");
-                    }break;
-                case 6:
-                    saturdaySalary = (double) (salaryPerMinutes * totaltimeWork);
-                    System.out.println("Start uur " + "     \t     " + "Stop uur " + "     \t     " + "Startday " + "     \t     " + "Enday" + "     \t          " + "uur gewerkt" + "     \t      " + "Loon");
-                    System.out.print(startHour + "u" + startminutes + "m"+ "     \t           " + stophours + "u" + startminutes + "m"+ "     \t       " + "Saturday" + "      \t     " + "Saturday" + "       \t        " + worktotalhour + "u"+ totalminutes + "m"+ "     \t	      " + saturdaySalary/100+ "€");
-                    break;
-                    case 7:
-                        sundaySalary = (double) (salaryPerMinutes * totaltimeWork);
-                        System.out.println("Start uur " + "     \t     " + "Stop uur " + "     \t     " + "Startday " + "     \t     " + "Enday" + "     \t          " + "uur gewerkt" + "     \t      " + "Loon");
-                        System.out.print(startHour + "u" + startminutes + "m"+ "     \t           " + stophours + "u" + startminutes + "m"+ "     \t       " + "Sunday" + "      \t     " + "Sunday" + "       \t        " + worktotalhour + "u"+ totalminutes + "m"+ "     \t	      " + sundaySalary/100+ "€");
-                        break;
-                default:
-                    throw new IllegalStateException("Unexpected value: " + day);
+        } else {
+            if(stophours < startHour ) {
+                totaltimeWorkBasic = (24-startHour)*60 - startminutes;
+                salaryBasic = (double) ((salaryPerMinuteSunday) * totaltimeWorkBasic);
+                totalTimeOvertime = stophours * 60 + stopminute;
+                salaryOvertime = (double) ((salaryPerMinutes) * totalTimeOvertime);
+                totalSalary = salaryBasic+salaryOvertime;
+            }else {
+                totaltimeWork = worktotalhour * 60 + totalminutes;
+                totalSalary = (double) ((salaryPerMinuteSunday) * totaltimeWork);
             }
         }
+        System.out.println("Start uur " + "     \t     " + "Stop uur " + "    \t    " + "Startday " + "     \t   " + "uur gewerkt" + "     \t      " + "Loon");
+        System.out.print(startHour + "u" + startminutes + "m"+"     \t       " + stophours + "u" + startminutes + "m"+ "    \t     " + "Monday" + "    \t        " + worktotalhour + "u"+ totalminutes + "m"+  "       \t    " +String.format("%.2f", totalSalary)+"€");
     }
+}
+
 
