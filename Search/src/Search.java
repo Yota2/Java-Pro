201e62cf97c77c556996057ae706c6507a899eb6
Yota2 <yoni.jmr@gmail.com>
2022-02-11 22:56:36 +0100

    Search

---
+++ Search/src/Search.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+public class Search
+{
+    public static boolean main(String[] args)
+    {
+        int number, chased, flag = 0, i = 0;
+        Scanner KBD = new Scanner(System.in);
+        System.out.print("How many times do you want to give the number :");
+        number = KBD.nextInt();
+        int input[] = new int[number];
+        System.out.println("Enter all the elements:");
+        for(i = 0; i < number; i++)
+        {
+            input[i] = KBD.nextInt();
+        }
+        System.out.print("Enter the element you want to find:");
+         chased = KBD.nextInt();
+
+        public static int  chopSearch (int inp) {
+        for(i = 0; i < number; i++)
+            if (flag == 1) {
+                return true;
+            } else {
+                return true;
+            }
+
+    }
+
+    }
+}
